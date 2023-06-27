cda1d81700303370b32a417f3255fb66bc640f42
Takuya Hirata <takuyahirata4@gmail.com>
2023-06-27 19:27:49 -0400

    Feat: update core components

--- lib/trackdays_web/components/core_components.ex
+++ lib/trackdays_web/components/core_components.ex
@@ -196,7 +196,7 @@ defmodule TrackdaysWeb.CoreComponents do
   def simple_form(assigns) do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
-      <div class="mt-10 space-y-8 bg-white">
+      <div>
         <%= render_slot(@inner_block, f) %>
         <div :for={action <- @actions} class="mt-2 flex items-center justify-between gap-6">
           <%= render_slot(action, f) %>
@@ -389,7 +389,7 @@ defmodule TrackdaysWeb.CoreComponents do
 
   def label(assigns) do
     ~H"""
-    <label for={@for} class="block text-sm font-semibold leading-6 text-zinc-800">
+    <label for={@for} class="block text-sm font-semibold leading-6">
       <%= render_slot(@inner_block) %>
     </label>
     """
--- lib/trackdays_web/live/admin/login_live.ex
+++ lib/trackdays_web/live/admin/login_live.ex
@@ -4,10 +4,11 @@ defmodule TrackdaysWeb.Admin.LoginLive do
   def render(assigns) do
     ~H"""
     <div>
-      <h1>Log in</h1>
       <.simple_form for={@form} id="login_form" action={~p"/admin/log_in"} phx-update="ignore">
-        <.input field={@form[:email]} type="email" label="Email" required />
-        <.input field={@form[:password]} type="password" label="Password" required />
+        <div class="flex flex-col gap-y-6">
+          <.input field={@form[:email]} type="email" label="Email" required />
+          <.input field={@form[:password]} type="password" label="Password" required />
+        </div>
 
         <:actions>
           <.button phx-disable-with="Signing in..." class="w-full">
