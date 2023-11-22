cd128da3d88ba7e2052e83c17e02dc5815b5e8e8
Takuya Hirata <takuyahirata4@gmail.com>
2023-11-22 13:41:53 -0500

    Chore: trackdays and organizations migration

---
+++ priv/repo/migrations/20231122182619_add_trackdays_and_organizations.exs
@@ -0,0 +1,26 @@
+defmodule Trackdays.Repo.Migrations.AddTrackdaysAndOrganizations do
+  use Ecto.Migration
+
+  def change do
+    create table(:organizations, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string, size: 50, null: false
+      add :trackdays_registration_url, :string
+
+      timestamps()
+    end
+
+    create table(:trackdays, primary_key: false) do
+      add(:id, :binary_id, primary_key: true)
+      add :date, :date, null: false
+      add :price, :integer, null: false
+      
+      add :track_id, references(:tracks, type: :binary_id, on_delete: :nothing, null: false)
+      add :organization_id, references(:organizations, type: :binary_id, on_delete: :nothing, null: false)
+      timestamps()
+    end
+
+    create(index(:trackdays, [:date]))
+    create(unique_index(:trackdays, [:organization_id, :date], name: :trackdays_constraint))
+  end
+end
