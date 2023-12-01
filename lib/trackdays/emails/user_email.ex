a518344db26470da2a47f8464eda4fbad835f657
Takuya Hirata <takuyahirata4@gmail.com>
2023-11-30 20:39:29 -0500

    Config: email template id

--- lib/trackdays/emails/user_email.ex
+++ lib/trackdays/emails/user_email.ex
@@ -1,23 +1,25 @@
 defmodule Trackdays.Emails.UserEmail do
   import Swoosh.Email
 
-  use Phoenix.Swoosh,
-    template_root: "lib/trackdays_web/emails",
-    template_path: "users"
+  # use Phoenix.Swoosh,
+  #   template_root: "lib/trackdays_web/emails",
+  #   template_path: "users"
 
+  # Signup Email Verification
   def welcome(user, verify_link) do
     new()
     |> from("support@motorcycle-trackdays.com")
     |> to(user.email)
-    |> put_provider_option(:template_id, 2)
-    |> put_provider_option(:params, %{link: verify_link})
+    |> put_provider_option(:template_id, 4)
+    |> put_provider_option(:params, %{link: verify_link, name: user.name})
   end
 
-  def new_email_verification(_name, new_email, verify_link) do
+  # Email Verification 
+  def new_email_verification(name, new_email, verify_link) do
     new()
     |> from("support@motorcycle-trackdays.com")
     |> to(new_email)
     |> put_provider_option(:template_id, 2)
-    |> put_provider_option(:params, %{link: verify_link})
+    |> put_provider_option(:params, %{link: verify_link, name: name})
   end
 end
