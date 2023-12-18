24ec25d07607ed77c2d842716ecfcc25cd21e9f0
Takuya Hirata <takuyahirata4@gmail.com>
2023-12-18 07:52:58 -0500

    Test: dubugging aws

--- lib/trackdays_web/controllers/image_controller.ex
+++ lib/trackdays_web/controllers/image_controller.ex
@@ -4,13 +4,21 @@ defmodule TrackdaysWeb.ImageController do
   alias Trackdays.Accounts
 
   def profile(conn, %{"image" => image}) do
+  IO.inspect(System.get_env("AWS_ACCESS_KEY_ID"))
+  IO.inspect(System.get_env("AWS_SECRET_ACCESS_KEY"))
+
     bucket_name = "trackdays-proto"
     filename = "#{conn.assigns.current_user.id}-profile.jpg"
+
+    IO.inspect("Start AWS functions")
     image = 
       image.path
       |> ExAws.S3.Upload.stream_file()
       |> ExAws.S3.upload(bucket_name, filename)
       |> ExAws.request()
+    IO.inspect("End AWS functions")
+
+    IO.inspect(image, label: "Response from AWS")
 
     with  {:ok, res} <- image,
          {:ok, _user} <- Accounts.upsert_profile_image_url(res.body.location, conn.assigns.current_user) do
