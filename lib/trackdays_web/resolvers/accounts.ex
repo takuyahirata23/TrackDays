90e2f413ba3f8414239abaed7066d7ea044d270f
Takuya Hirata <takuyahirata4@gmail.com>
2023-12-19 18:14:45 -0500

    Feat: update group mutation

--- lib/trackdays_web/resolvers/accounts.ex
+++ lib/trackdays_web/resolvers/accounts.ex
@@ -8,4 +8,11 @@ defmodule TrackdaysWeb.Resolvers.Accounts do
   def get_groups(_, _, _) do
     {:ok, Accounts.get_groups()}
   end
+
+  def update_group(_, %{group_id: group_id}, %{context: %{current_user: current_user}}) do
+    case Accounts.update_group(current_user, group_id) do
+      {:error, _} -> {:error, message: "Updating group failed"}
+      {:ok, user} -> {:ok, user}
+    end
+  end
 end
