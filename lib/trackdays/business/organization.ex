758c92c4d4fc2765fb331890b78e8d27a189d1a0
Takuya Hirata <takuyahirata4@gmail.com>
2024-01-30 22:06:09 -0500

    Feat: default note migration

--- lib/trackdays/business/organization.ex
+++ lib/trackdays/business/organization.ex
@@ -9,6 +9,7 @@ defmodule Trackdays.Business.Organization do
     field :name, :string
     field :trackdays_registration_url, :string
     field :homepage_url, :string
+    field :default_note, :string
 
     has_many :trackdays, Trackdays.Event.Trackday, preload_order: [asc: :start_datetime]
 
@@ -17,10 +18,11 @@ defmodule Trackdays.Business.Organization do
 
   def changeset(organization, attrs \\ %{}) do
     organization
-    |> cast(attrs, [:name, :trackdays_registration_url, :homepage_url])
+    |> cast(attrs, [:name, :trackdays_registration_url, :homepage_url, :default_note])
     |> validate_required([:name])
     |> validate_length(:name, min: 2, max: 30)
     |> validate_length(:homepage_url, min: 2, max: 50)
+    |> validate_length(:default_note, min: 2)
     |> unique_constraint([:name],
       name: :organazation_name_constraint,
       message: "Organization already exists"
