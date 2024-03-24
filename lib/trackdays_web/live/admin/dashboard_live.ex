0462bf3a224145ee3a7cf819f80c9582ae2a6f65
Takuya Hirata <takuyahirata4@gmail.com>
2024-03-24 13:35:02 -0400

    Feat: display total users registered

--- lib/trackdays_web/live/admin/dashboard_live.ex
+++ lib/trackdays_web/live/admin/dashboard_live.ex
@@ -1,13 +1,27 @@
 defmodule TrackdaysWeb.Admin.DashboardLive do
   use TrackdaysWeb, :live_view
 
+  alias Trackdays.Accounts
+
   def render(assigns) do
     ~H"""
-    <div>here</div>
+    <div>
+      <.card class="grid gap-y-4">
+        <p class="text-center font-bold text-2xl">Numbers</p>
+        <.card variant="secondary" class="flex flex-col items-center gap-y-4">
+          <p class="font-bold">Total Users</p>
+          <span>
+            <%= @total_users %>
+          </span>
+        </.card>
+      </.card>
+    </div>
     """
   end
 
   def mount(_, _, socket) do
-    {:ok, socket}
+    total_users = Accounts.count_users()
+
+    {:ok, assign(socket, total_users: total_users)}
   end
 end
